7208341ec1737ca94aec7b50e445eb02dd353c92
吴建昆 <wujiankun@corp.netease.com>
2022-04-28 14:27:39 +0800

    init-proj

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    globals: {
+        defineProps:"readonly",
+        defineEmits:"readonly",
+        defineExpose:"readonly",
+        withDefaults:"readonly",
+    },
+    "extends": [
+        "plugin:vue/essential",
+        "standard"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}

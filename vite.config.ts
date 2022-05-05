a94cca00b877e8af953b11bfaf5eb9d904673904
wjk19 <wjk333@yeah.net>
2022-05-05 23:17:17 +0800

    remove:unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import VitePluginEslint from 'vite-plugin-eslint'
 import VueJsx from '@vitejs/plugin-vue-jsx'
 import * as path from 'path'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
 export default defineConfig({
   plugins: [
     vue(),
@@ -13,19 +10,7 @@ export default defineConfig({
       // vite 编译过程中的 lint 可配置于此
       // failOnError: false
     }),
-    VueJsx({}),
-    Components({
-      // allow auto load markdown components under `./src/components/`
-      extensions: ['vue', 'md'],
-      // allow auto import and register components used in markdown
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      resolvers: [
-        ElementPlusResolver({
-          importStyle: 'sass'
-        })
-      ],
-      dts: 'src/components.d.ts'
-    })
+    VueJsx({})
   ],
   resolve: {
     alias: {
